ec78cc7fef7d693f4fe8afbfa325bb1bee774606
MrSpooky <43094600+EvgenyMendeleev@users.noreply.github.com>
2023-10-26 11:25:47 +0300

    Changing pages sound at the calendar was added

--- Assets/Scripts/GUI/CalendarManager.cs
+++ Assets/Scripts/GUI/CalendarManager.cs
@@ -7,10 +7,13 @@ public class CalendarManager : MonoBehaviour
     private Text _textMonth;
     public int MonthNumber { get; private set; }
 
+    private AudioSource _audioSource;
+
     private void Start()
     {
         MonthNumber = Random.Range(1, 13);
         _textMonth.text = MonthNumberConvertToString(MonthNumber);
+        _audioSource = GetComponent<AudioSource>();
     }
 
     public void NextMonth()
@@ -18,6 +21,7 @@ public class CalendarManager : MonoBehaviour
         MonthNumber++;
         if (MonthNumber > 12) MonthNumber = 1;
         _textMonth.text = MonthNumberConvertToString(MonthNumber);
+        _audioSource.Play();
     }
 
     public void PreviousMonth()
@@ -25,6 +29,7 @@ public class CalendarManager : MonoBehaviour
         MonthNumber--;
         if (MonthNumber < 1) MonthNumber = 12;
         _textMonth.text = MonthNumberConvertToString(MonthNumber);
+        _audioSource.Play();
     }
 
     private string MonthNumberConvertToString(int month)
