29fa28a017b54c989687f217e28a3b6566cbe2a1
Ivan20030 <59804947+Ivan20030@users.noreply.github.com>
2023-10-31 17:31:21 +0300

    PopitkaInput

--- Assets/Scripts/SceneText.cs
+++ Assets/Scripts/SceneText.cs
@@ -1,13 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 public class SceneText : MonoBehaviour
 {
     public GameObject HeadText;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)){
+        if (InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(CommonUsages.secondary2DAxis, out Vector2 axisValue)){// || Input.GetKeyDown(KeyCode.W)){
             if (HeadText.activeInHierarchy)
             {
                 HeadText.SetActive(false);
