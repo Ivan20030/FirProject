94e4ef234342447cb64807849114633d3d205122
Ivan20030 <59804947+Ivan20030@users.noreply.github.com>
2023-11-01 14:24:47 +0300

    update

--- Assets/Scripts/SceneText.cs
+++ Assets/Scripts/SceneText.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
 
 public class SceneText : MonoBehaviour
 {
     public GameObject HeadText;
+
+    public InputActionReference A;
     private void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.One) && HeadText.activeInHierarchy)
+        if (OVRInput.GetDown(OVRInput.Button.One) && A)
         {
-            HeadText.SetActive(false);
-        }
-        else if(OVRInput.GetDown(OVRInput.Button.One) && !HeadText.activeInHierarchy)
-        {
-            HeadText.SetActive(true);
+            if (HeadText.activeInHierarchy)
+            {
+                HeadText.SetActive(false);
+            }
+            else
+            {
+                HeadText.SetActive(true);
+            }
         }
     }
 }
