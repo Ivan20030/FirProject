7e41fb6c0ee378943da6f3b67b9370c2c975c5fa
MrSpooky <43094600+EvgenyMendeleev@users.noreply.github.com>
2023-11-13 16:15:15 +0300

    Seeds spawn mechanic was added

--- Assets/Scripts/Cone/SeedSpawnComponent.cs
+++ Assets/Scripts/Cone/SeedSpawnComponent.cs
@@ -1,6 +1,12 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public struct SeedSpawn
+{
+    public GameObject _cone;
+    public float _spawnTime;
+}
 
 public class SeedSpawnComponent : MonoBehaviour
 {
@@ -8,13 +14,40 @@ public class SeedSpawnComponent : MonoBehaviour
     private GameObject _seed;
     [SerializeField]
     private int _seedCount;
+    [SerializeField]
+    private Transform _seedSpawnPoint;
+    [SerializeField]
+    private GameObject _spawnEffect;
+    [SerializeField]
+    private string _coneTag;
+    [SerializeField]
+    private float _deltaSpawnTime = 3.0f;
 
-    void SpawnSeeds(int SeedCount)
+    private Queue<SeedSpawn> _seedSpawns = new Queue<SeedSpawn>();
+
+    private void Update()
     {
-        for (int i = 0; i < SeedCount; i++)
+        if (_seedSpawns.Count <= 0) return;
+        
+        if (Time.time > _seedSpawns.Peek()._spawnTime)
         {
-            //TODO (optional): Perhaps there is a need to add the disappearance of the cone.
-            Instantiate(_seed, transform.position, Quaternion.identity);
+            var seedSpawn = _seedSpawns.Dequeue();
+            Destroy(seedSpawn._cone);
+            Instantiate(_spawnEffect, _seedSpawnPoint.position, Quaternion.identity);
+            for (int i = 0; i < _seedCount; i++)
+            {
+                Instantiate(_seed, _seedSpawnPoint.position, Quaternion.identity);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!collider.TryGetComponent(out XRGrabInteractable grabComponent)) return;
+        if (collider.tag == _coneTag && !grabComponent.isSelected)
+        {
+            var seedSpawn = new SeedSpawn { _cone = collider.gameObject, _spawnTime = Time.time + _deltaSpawnTime };
+            _seedSpawns.Enqueue(seedSpawn);
         }
     }
 }
