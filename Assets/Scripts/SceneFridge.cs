eae705d7c6477dd13f9ce162fe8fb12db5ed2026
Ivan20030 <59804947+Ivan20030@users.noreply.github.com>
2023-11-14 07:03:35 +0300

    TextEnd
    1) Доделал оформление изображения на канвасе
    2) Сделал изменение текста при открытии/закрытии холодильника

--- Assets/Scripts/SceneFridge.cs
+++ Assets/Scripts/SceneFridge.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class SceneFridge : MonoBehaviour
 {
     bool IsOpened = false;
+    public TMP_Text Text1;
     public void OnClick(SelectEnterEventArgs context)
     {
         if (!IsOpened)
         {
             transform.rotation = Quaternion.Euler(0,150,0);
+            Text1.SetText("Нажмите, чтобы закрыть");
             IsOpened = true;
         }
         else
         {
             transform.rotation = Quaternion.Euler(0, 297, 0);
+            Text1.SetText("Нажмите, чтобы открыть");
             IsOpened = false;
         }
     }
